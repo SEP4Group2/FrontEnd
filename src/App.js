08875103437a406caa15654ca3ff0b41b0a9158b
Sedilin <sramkowskilukasz@gmail.com>
2023-11-09 15:57:27 +0100

    Clean up App and components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import './App.css';
-import TopBar from './components/Topbar';
-import Navbar from './components/Navbar';
-import PlantList from './components/PlantList';
 function App() {
   return (
     <div>
-      <TopBar />
-      <div className="content">
-        <Navbar />
-        <PlantList />
         {/* Your main content goes here */}
-      </div>
     </div>
   );
 }
