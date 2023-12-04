7d002b9e627db4e40169829502402dafeee44148
DanieliusNev <104202615+DanieliusNev@users.noreply.github.com>
2023-12-04 21:18:46 +0100

    Adjustment on graphics

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -1,82 +1,101 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ApexCharts from "react-apexcharts";
 
 const Analytics = () => {
-  const dummyData1 = [
-    { x: "2023-12-04T00:00:00", y: 20 },
-    { x: "2023-12-04T01:00:00", y: 30 },
-    // Add more data as needed
-  ];
-
-  const dummyData2 = [
-    { x: "2023-12-04T00:00:00", y: 40 },
-    { x: "2023-12-04T01:00:00", y: 50 },
-    // Add more data as needed
-  ];
-
-  const dummyData3 = [
-    { x: "2023-12-04T00:00:00", y: 60 },
-    { x: "2023-12-04T01:00:00", y: 70 },
-    // Add more data as needed
-  ];
-
-  const dummyData4 = [
-    { x: "2023-12-04T00:00:00", y: 80 },
-    { x: "2023-12-04T01:00:00", y: 90 },
-    // Add more data as needed
-  ];
-
-  const [chartData, setChartData] = useState([
+  const chartData = [
     {
-      series: [{ data: dummyData1 }],
-      options: { chart: { id: 'fb', group: 'social', type: 'line', height: 160 }, colors: ['#008FFB'] },
+      id: 'humidity',
+      name: 'Humidity',
+      series: [
+        { x: "Nov 04 00", y: 40 },
+        { x: "Nov 04 01", y: 50 },
+        { x: "Nov 04 02", y: 45 },
+        { x: "Nov 04 03", y: 55 },
+        { x: "Nov 04 04", y: 60 },
+        { x: "Nov 04 05", y: 65 },
+        // Add more data as needed
+      ],
+      color: '#FF5733',
     },
     {
-      series: [{ data: dummyData2 }],
-      options: { chart: { id: 'tw', group: 'social', type: 'line', height: 160 }, colors: ['#546E7A'] },
+      id: 'moisture',
+      name: 'Moisture',
+      series: [
+        { x: "Nov 04 00", y: 60 },
+        { x: "Nov 04 01", y: 70 },
+        { x: "Nov 04 02", y: 65 },
+        { x: "Nov 04 03", y: 75 },
+        { x: "Nov 04 04", y: 80 },
+        { x: "Nov 04 05", y: 40 },
+        // Add more data as needed
+      ],
+      color: '#33FF57',
     },
     {
-      series: [{ data: dummyData3 }],
-      options: { chart: { id: 'yt', group: 'social', type: 'area', height: 160 }, colors: ['#00E396'] },
+      id: 'temperature',
+      name: 'Temperature',
+      series: [
+        { x: "Nov 04 00", y: 80 },
+        { x: "Nov 04 01", y: 90 },
+        { x: "Nov 04 02", y: 85 },
+        { x: "Nov 04 03", y: 95 },
+        { x: "Nov 04 04", y: 100 },
+        { x: "Nov 04 05", y: 57 },
+        // Add more data as needed
+      ],
+      color: '#5733FF',
     },
     {
-      series: [{ data: dummyData4 }],
-      options: { chart: { id: 'ig', group: 'social', type: 'area', height: 160, width: 300 }, colors: ['#008FFB'] },
+      id: 'uv-light',
+      name: 'UV Light',
+      series: [
+        { x: "Nov 04 00", y: 100 },
+        { x: "Nov 04 01", y: 110 },
+        { x: "Nov 04 02", y: 105 },
+        { x: "Nov 04 03", y: 115 },
+        { x: "Nov 04 04", y: 120 },
+        { x: "Nov 04 05", y: 112 },
+        // Add more data as needed
+      ],
+      color: '#FFE833',
     },
-  ]);
-
-  useEffect(() => {
-    // Set up an interval to update the charts every 5000 milliseconds (5 seconds)
-    const intervalId = setInterval(() => {
-      // Dummy data remains static, no need to update
-    }, 5000);
-
-    // Cleanup interval on component unmount
-    return () => clearInterval(intervalId);
-  }, []);
+  ];
 
   return (
     <div>
       {chartData.map((chart, index) => (
         <div key={index}>
+          <h3>{chart.name}</h3>
           <ApexCharts
             options={{
-              ...chart.options,
               xaxis: {
-                type: "datetime",
+                type: "category",
+                categories: chart.series.map(point => point.x),
                 labels: {
-                  datetimeFormatter: {
-                    month: "MMM",
-                    day: "dd",
-                    hour: "HH",
-                    minute: "mm",
+                  formatter: function (value) {
+                    return value && !value.endsWith(" ") ? `${value}h` : value;
                   },
                 },
               },
+              fill: {
+                type: 'gradient',
+                gradient: {
+                  shadeIntensity: 1,
+                  opacityFrom: 0.95,
+                  opacityTo: 0.94,
+                  stops: [0, 100],
+                },
+              },
+              colors: [chart.color],
             }}
-            series={chart.series}
-            type={chart.options.chart.type}
-            height={chart.options.chart.height}
+            series={[
+              {
+                name: chart.id,
+                data: chart.series.map(point => ({ x: point.x, y: point.y })),
+              },
+            ]}
+            type="area"
+            height={160}
           />
         </div>
       ))}
