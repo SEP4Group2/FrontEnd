8815aa5fbb72a3e0c64d8b655a7a771b1ddb9c07
GabrielaDicusar <gabrieladicusar@gmail.com>
2023-12-05 16:22:42 +0100

    Added the loading const

--- src/App.js
+++ src/App.js
@@ -12,13 +12,16 @@ const App = () => {
   const [token, setToken] = useState();
   const [plants, setPlants] = useState([]);
   const [plantsData, setPlantsData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (token) {
       const fetchPlants = async () => {
         try {
+          
           const plantResponse = await axios.get("http://localhost:5000/Plant");
           setPlants(plantResponse.data);
+          setLoading(false);
         } catch (error) {
           console.error("Error fetching plants:", error);
         }
@@ -36,7 +39,7 @@ const App = () => {
         }
       };
 
-      const fetchInterval = setInterval(fetchPlants, 5 * 60 * 1000); // Fetch plants every 5 minutes
+      const fetchInterval = setInterval(fetchPlants, 1000); // Fetch plants every 5 minutes
       const dataInterval = setInterval(fetchPlantsData, 5000); // Fetch data every 5 seconds
 
       // Cleanup intervals on component unmount
@@ -59,7 +62,7 @@ const App = () => {
               <>
                 <Route
                   path="/myPlants"
-                  element={<PlantList plants={plants} plantsData={plantsData} />}
+                  element={<PlantList plants={plants} plantsData={plantsData} loading={loading}/>}
                 />
                 <Route path="/newPlant" element={<RegisterPlant />} />
                 <Route path="/" element={<Navigate to="/myPlants" />} />
