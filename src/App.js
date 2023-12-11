9012e5668dee6df098a722474023866a186e5ea2
Sedilin <sramkowskilukasz@gmail.com>
2023-12-11 12:31:26 +0100

    Fixed delete profile method

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ const App = () => {
             <Route path="/loadPage" element={<LandingPage/>}/>
             {token ? (
               <>
-                <Route path="/myProfile" element={<MyProfile user={user} setUser={setUser} />} />
+                <Route path="/myProfile" element={<MyProfile user={user} setUser={setUser} setToken={setToken} />} />
                 <Route
                   path="/myPlants"
                   element={<PlantList plants={plants} userId={user.userId} plantsData={plantsData} loading={loading} />}
