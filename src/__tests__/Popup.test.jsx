24363ba48969c22041652e190f4b522817fab301
Sedilin <sramkowskilukasz@gmail.com>
2023-12-11 08:25:45 +0100

    Test coverage done

---
+++ src/__tests__/Popup.test.jsx
@@ -0,0 +1,51 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import Popup from "../components/Popup/Popup";
+
+describe("Rendering", () => {
+    it('should update state in Popup when input fields are changed and generate correct JSON object', () => {
+        render(<Popup onCancel={() => { }} userId="1" />);
+
+        jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+            status: 201,
+            json: jest.fn().mockResolvedValue({}),
+        });
+
+        fireEvent.change(screen.getByLabelText(/Type:/i), { target: { value: 'CustomType' } });
+        fireEvent.change(screen.getByLabelText(/Humidity level:/i), { target: { value: '50' } });
+        fireEvent.change(screen.getByLabelText(/Moisture level:/i), { target: { value: '60' } });
+        fireEvent.change(screen.getByLabelText(/Temperature level:/i), { target: { value: '25' } });
+        fireEvent.change(screen.getByLabelText(/Light level:/i), { target: { value: '500' } });
+
+        fireEvent.click(screen.getByTestId("save-button"));
+
+        const expectedPresetData = {
+            userId: '1',
+            name: 'CustomType',
+            humidity: '50',
+            moisture: '60',
+            temperature: '25',
+            uvLight: '500',
+        };
+        
+        expect(window.fetch).toHaveBeenCalledWith(
+            'http://localhost:5000/PlantPreset/createPlantPreset',
+            expect.objectContaining({
+                method: 'POST',
+                body: JSON.stringify(expectedPresetData),
+            })
+        );
+    });
+
+    it('should trigger actions when buttons in Popup are clicked', () => {
+        const onCancelMock = jest.fn();
+        render(<Popup onCancel={onCancelMock} userId="1" />);
+
+        // Simulate clicking on the "Cancel" button
+        fireEvent.click(screen.getByText(/Cancel/i));
+
+        // Assert that the onCancel function is called
+        expect(onCancelMock).toHaveBeenCalledTimes(1);
+
+    });
+})
--- src/__tests__/RegisterPlant.test.jsx
+++ src/__tests__/RegisterPlant.test.jsx
@@ -1,49 +1,45 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom';
-import NewPlant from "../components/RegisterPlant/RegisterPlant";
+import RegisterPlant from "../components/RegisterPlant/RegisterPlant";
 
 describe("Rendering", () => {
-    it('should render preset component when "Other" is chosen', async () => {
-        render(<NewPlant onCancel={() => { }} />);
+    it('should render RegisterPlant component with initial state and select element', async () => {
+        render(<RegisterPlant onCancel={() => { }} userId="1" />);
 
-        // Get the select element
-        const typeSelect = screen.getByLabelText(/Type:/i);
-
-        // Select the "Other" option
-        fireEvent.change(typeSelect, { target: { value: 'Other' } });
-
-        // Assert that the Popup component is rendered
-        expect(screen.getByTestId("popup")).toBeInTheDocument();
-    });
-
-    it('should render NewPlant component with initial state and select element', async () => {
-        render(<NewPlant onCancel={() => { }} />);
-
-        // Check if the component renders without crashing
-        expect(screen.getByText('Register a New Plant')).toBeInTheDocument();
-
-        // Check if the initial state is set correctly
-        expect(screen.getByLabelText(/Name:/i).value).toBe('');
-        expect(screen.getByLabelText(/Location:/i).value).toBe('');
-        expect(screen.getByLabelText(/Type:/i).value).toBe('');
-        // Add more assertions for other fields as needed
-
-        // Verify that the select element is present and contains the "Other" option
-        const typeSelect = screen.getByLabelText(/Type:/i);
-        expect(typeSelect).toBeInTheDocument();
+        expect(screen.getByLabelText(/Device ID:/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Name:/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Humidity Level:/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Moisture Level:/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Light Level:/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Temperature Level:/i)).toBeInTheDocument();
+        expect(screen.getByAltText(/Main Plant Icon/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Type:/i)).toBeInTheDocument();
         expect(screen.getByText(/Select Type/i)).toBeInTheDocument();
     });
 });
-describe("State Update", () => {
+describe("Update State", () => {
     it('should update state when input fields are changed', () => {
-        render(<NewPlant onCancel={() => { }} />);
+        render(<RegisterPlant onCancel={() => { }} userId="1" />);
 
-        // Simulate user input for Name and Location fields
         fireEvent.change(screen.getByLabelText(/Name:/i), { target: { value: 'Test Plant' } });
         fireEvent.change(screen.getByLabelText(/Location:/i), { target: { value: 'Living Room' } });
 
-        // Assert that state is updated accordingly
         expect(screen.getByLabelText(/Name:/i).value).toBe('Test Plant');
         expect(screen.getByLabelText(/Location:/i).value).toBe('Living Room');
+    });
+});
+describe("Generate Component", () => {
+    it('should render Popup component when "Other" is chosen', async () => {
+        render(<RegisterPlant onCancel={() => { }} userId="1" />);
+
+        fireEvent.change(screen.getByLabelText(/Type:/i), { target: { value: 'Other' } });
+
+        expect(screen.getByTestId("popup")).toBeInTheDocument();
+    });
+    it('should display warning alert when there are empty fields', () => {
+        render(<RegisterPlant onCancel={() => { }} userId="1" />);
+
+        fireEvent.click(screen.getByText(/Save/i));
+        expect(screen.getByText(/Fields should not be empty!/i)).toBeInTheDocument();
     });
 });
